4309fbfb471134e3f3216665c0a62838aeebd338
ajujawade <ajayjawade06@gmail.com>
2024-03-05 20:13:09 +0530

    string

---
+++ script17.js
@@ -0,0 +1,83 @@
+let city = "chandrapur"
+
+console.log(city)
+
+
+let q1 = city.slice(2)
+console.log(q1)
+
+let q2 = city.slice(2, 8)
+console.log(q2)
+
+console.log(city.slice(2, -7))
+console.log(city.slice(-8))
+console.log(city.slice(4, 6))
+console.log(city.slice(1, -1))
+console.log(city.slice(-1, -3))
+
+//Program 2
+
+let lastName = "Jawade"
+let fullName = lastName.concat(" Ajay")
+
+console.log(fullName)
+console.log(lastName.concat(" ").concat("Ajay"))
+
+// Program 3
+
+fullName = "ajay jawade 7972235361"
+
+info = fullName.split(" ")
+console.log(info)
+console.log(fullName.split("a"))
+
+//Program 4
+
+let city2 = "nagpur"
+let e2 = city2.indexOf('g')
+console.log(e2)
+
+let city3 = "chandrapur"
+
+let e3 = city.lastIndexOf("a")
+console.log(e3)
+
+let info2 = "I am learning js"
+
+let e4 = info2.replace("js", "python")
+
+console.log(e4)
+
+let e5 = info2.repeat(2)
+console.log(e5)
+
+
+//Program 5
+
+let city4 = "warDha"
+
+console.log(city4.charAt(1))
+console.log(city.charCodeAt(3))
+
+
+
+let info5 = "i am learning javascript"
+
+console.log(info2.substring(2, 88))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
