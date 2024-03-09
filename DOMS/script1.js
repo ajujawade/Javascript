9b068d22389e766c53f0dc6570a1365f5845c369
ajujawade <ajayjawade06@gmail.com>
2024-03-09 19:10:42 +0530

    added

---
+++ DOMS/script1.js
@@ -0,0 +1,49 @@
+//<h2 id = "four" class = "five">Ajay</h2>
+
+
+//by tag name
+
+let q1 = document.querySelector("h2")
+console.log(q1)
+
+
+// by id
+
+let q2 = document.querySelector('#three')
+console.log(q2)
+
+// by class
+
+let q3 = document.querySelector('.four')
+console.log(q3)
+
+//by attribute
+// tagname[attribute = "value"]
+
+let q4 = document.querySelector('h2[class ="four"]')
+console.log(q4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
