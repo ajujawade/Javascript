ceab09feaaacb728ebeaa532aac3943c317ab735
ajujawade <ajayjawade06@gmail.com>
2024-03-18 22:40:08 +0530

    added

---
+++ DOMS/script9.js
@@ -0,0 +1,29 @@
+let headone = document.querySelector('#one')
+console.log(headone)
+
+let headtwo = document.querySelector('.two')
+console.log(headtwo)
+
+let listLi = document.querySelectorAll('li')
+console.log(listLi)
+
+let listLi2 = document.getElementsByTagName('li')
+console.log(listLi2)
+
+let className = document.querySelectorAll('.fruits')
+console.log(className)
+
+let className2 = document.getElementsByClassName('fruits')
+console.log(className2)
+
+let a = document.getElementById("one")
+console.log(a)
+
+let ByAttr = document.querySelector("li[name = nm]")
+console.log(ByAttr)
+
+let ByAttr2 = document.getElementsByName('nm')
+console.log(ByAttr2)
+
+
+//
